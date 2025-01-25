10993842fb6081de7382f27049181faa7be2fc62
S8A <samuelochoap@gmail.com>
2025-01-25 10:34:45 -0400

    Use postcss-url to copy Bootstrap Icons font files on build

--- postcss.config.js
+++ postcss.config.js
@@ -1,6 +1,16 @@
+import path from "path";
+
 export default {
     plugins: {
         "@tailwindcss/postcss": {},
+        "postcss-url": [
+            {
+                filter: /bootstrap-icons\.woff.?$/,
+                url: "copy",
+                basePath: path.resolve("node_modules/bootstrap-icons/font"),
+                assetsPath: "assets",
+            },
+        ],
         autoprefixer: {},
         ...(
             process.env.JEKYLL_ENV === "production" 

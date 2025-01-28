a9309464b27b697010f059f7318912db0cda0ee8
S8A <samuelochoap@gmail.com>
2025-01-28 11:36:06 -0400

    Implement dark mode using Alpine store, localStorage, & matchMedia API

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,4 +2,19 @@ import Alpine from "alpinejs";
 
 window.Alpine = Alpine;
 
+Alpine.store("darkMode", {
+    on: false,
+    init() {
+        if ("theme" in localStorage) {
+            this.on = localStorage.theme === "dark";
+        } else {
+            this.on = window.matchMedia("(prefers-color-scheme: dark)").matches;
+        }
+    },
+    toggle() {
+        this.on = !this.on;
+        localStorage.theme = this.on ? "dark" : "light";
+    },
+});
+
 Alpine.start();

ba1194c5f8ca00301e41ddb93c913016b3e16b19
S8A <S8A@users.noreply.github.com>
2025-02-05 10:15:40 -0400

    deploy: 75f6b95acccd6fbbfe4a2fffe15009da1d491bfd

---
+++ postcss.config.js
@@ -0,0 +1,20 @@
+import path from "path";
+
+export default {
+    plugins: {
+        "@tailwindcss/postcss": {},
+        "postcss-url": [
+            {
+                filter: /bootstrap-icons\.woff.?$/,
+                url: "copy",
+                basePath: path.resolve("node_modules/bootstrap-icons/font"),
+                assetsPath: "assets",
+            },
+        ],
+        ...(
+            process.env.JEKYLL_ENV === "production" 
+            ? { cssnano: { preset: "default" } }
+            : {}
+        ),
+    },
+};

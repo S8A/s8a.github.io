7ec2638732ab8f877d5af7031fbab078d4990520
S8A <samuelochoap@gmail.com>
2025-04-25 11:00:23 -0400

    Remove & uninstall Bootstrap Icons

--- postcss.config.js
+++ postcss.config.js
@@ -5,12 +5,6 @@ export default {
         "@tailwindcss/postcss": {},
         "postcss-url": [
             {
-                filter: /bootstrap-icons\.woff.?$/,
-                url: "copy",
-                basePath: path.resolve("node_modules/bootstrap-icons/font"),
-                assetsPath: "..",
-            },
-            {
                 filter: /lucide\.(woff.?|ttf|svg|eot)$/,
                 url: "copy",
                 basePath: path.resolve("node_modules/lucide-static/font"),

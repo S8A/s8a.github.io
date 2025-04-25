a9b146942b92affd778317d411ff75555db82fc3
S8A <samuelochoap@gmail.com>
2025-04-25 15:25:33 -0400

    Workaround for Lucide icon fonts post Tailwind update

--- postcss.config.js
+++ postcss.config.js
@@ -7,8 +7,16 @@ export default {
             {
                 filter: /lucide\.(woff.?|ttf|svg|eot)$/,
                 url: "copy",
+                // Now Tailwind moves the font files so that they end up as
+                // ../../../node_modules/lucide-static/font/lucide.ext
                 basePath: path.resolve("node_modules/lucide-static/font"),
-                assetsPath: ".",
+                // Thus, if we set ../fonts it wont't end up as
+                // /assets/fonts/lucide.ext like we want, but rather as 
+                // /node_modules/lucide-static/font/lucide.ext, because
+                // the aforementioned relative path is such that it will
+                // go up three levels above /assets/fonts. That's why
+                // we set this dummy folder name
+                assetsPath: "./path-goes-up-three-levels-ignoring-this",
             },
         ],
     },
